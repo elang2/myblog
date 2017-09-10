e5efb42d44c0edceb7fb75f8e7656f37525ad47c
Elankumaran Srinivasan <elang2@gmail.com>
2017-09-10 08:37:05 -0700

    Adding more enhancements

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+/* 
+ * Configure the social media links
+ * */
+$(".fa-linkedin").attr("href", "https://www.linkedin.com/in/elansrini/");
+$(".fa-facebook").attr("href", "https://www.facebook.com/elang.kumaran.7");
+$(".fa-github").attr("href", "https://github.com/elang2");
+		
